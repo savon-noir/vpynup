51fdd9df691a33cce59b6ecb143e1dcdfa5aec26
Ronald <mini.pelle@gmail.com>
2013-10-20 11:53:53 +0200

    removed class stargate: todo: fix all the things

--- vpynup/stargate.py
+++ vpynup/stargate.py
@@ -8,136 +8,154 @@ from vpynup import provider
 from vpynup import fabricant
 
 
-class StarGate(object):
-    def __init__(self, config_file=None):
-        self.auth_params = None
-        self.instance_params = None
-        self.instance = None
+def _load_config(config_path=None):
+    dict_config = None
+    if config_path:
+        try:
+            with open(config_path) as jsonfd:
+                dict_config = json.load(jsonfd)
+        except Exception as e:
+            sys.stderr.write("json config loading failed:"
+                             " {0}\n".format(e.message))
+            sys.exit(1)
+    else:
+        sys.stderr.write("No config file provided or the file "
+                         "could not be found\n")
+        sys.exit(1)
 
-        self.conn = None
+    if not __validate_config(dict_config):
+        sys.stderr.write("Json config is not valid: some attributes "
+                         "are missing. Please check vpin documentation\n")
+        sys.exit(1)
 
-        self._load_config(config_file)
-        self.conn = provider.cloud_connect(self.auth_params)
+    return dict_config
 
-        if self.conn is None:
-            raise Exception("Connection to cloud provider failed")
+def __validate_config(dict_config):
+    rval = False
 
-    def _load_config(self, config_path=None):
+    if(dict_config and 'provider' in dict_config and
+       'auth' in dict_config['provider'] and
+       'instance' in dict_config['provider']):
+        rval = True
+
+    return rval
+
+def init():
+    rval = True
+    _cwd = os.getcwd()
+
+    _sdict = ({"provider": {
+                   "name": "aws",
+                   "auth": {
+                       "aws_access_key_id": "",
+                       "aws_secret_access_key": ""
+                   },
+                   "instance": {
+                       "image_id": "ami-c30360aa",
+                       "key_name": "",
+                       "key_path": ""
+                   }
+                 }
+              })
+
+    _aws_access_key_id = raw_input('enter/copy your amazon access key id: ')
+    _aws_secret_access_key = raw_input('enter/copy your amazon secret access id: ')
+    _aws_sshkey_name = raw_input('enter the name of your amazon ssh key: ')
+    _aws_sshkey_path = raw_input('enter the path to the corresponding private key (.pem file): ')
+
+    if '' not in [ _aws_access_key_id, _aws_secret_access_key, _aws_sshkey_name, _aws_sshkey_path ]:
+        _sdict['provider']['auth']['aws_access_key_id'] = _aws_access_key_id
+        _sdict['provider']['auth']['aws_secret_access_key'] = _aws_secret_access_key
+        _sdict['provider']['instance']['key_name'] = _aws_sshkey_name
+        _sdict['provider']['instance']['key_path'] = _aws_sshkey_path
+
+        try:
+            with open("{0}/{1}".format(_cwd), "vpinup.json") as jfd:
+                json.dump(_sdict, jfd, indent=4)
+        except IOError as e:
+            sys.stderr.write("File creation failed: {0}".format(e.message))
+            rval = False
+
+#            _env_dir = "{0}/{1}".format(_cwd, ".vpinup/")
+#            if not os.path.exists(_env_dir):
+#                try:
+#                    os.mkdir(_env_dir)
+#                except OSError as e:
+#                    sys.stderr.write("Environement directory creation failed: {0}".format(e.strerror))
+#                    rval = False
+    else:
+        sys.stderr.write("Not all required parameters have been provided.\n")
         rval = False
-        if config_path:
-            try:
-                with open(config_file) as jsonfd:
-                    dict_config = json.load(jsonfd)
-            except Exception as e:
-                sys.stderr.write("json config loading failed:"
-                                 " {0}\n".format(e.message))
-        else:
-            sys.stderr.write("No config file provided or the file "
-                             "could not be found\n")
+    return rval
 
-        if self.__validate_config(dict_config):
-            self.auth_params = dict_config['provider']['auth']
-            self.instance_params = dict_config['provider']['instance']
-            rval = True
-        else:
-            sys.stderr.write("Json config is not valid: some attributes "
-                             "are missing. Please check vpin documentation\n")
+def up():
+    _r = start()
+    if _r is not None:
+        print gate_hostname()
+        provision()
 
-        return rval
+    return _r
 
-    def __validate_config(self, dict_config):
-        rval = True
+def start(wait=True):
+    _config_file = "{0}/{1}".format(_cwd), "vpinup.json"
+    _configdict = _load_config(_config_file)
+    _auth_params = _config_dict['provider']['auth']
+    _instance_params = _config_dict['provider']['instance']
+    
+    _instance = None
+    conn = provider.cloud_connect(**_auth_params)
 
-        if(dict_config and 'provider' in dict_config and
-           'auth' in dict_config['provider']):
-            self.auth_params = dict_config['provider']['auth']
-        else:
-            rval = False
-
-        if(dict_config and 'provider' in dict_config and
-           'instance' in dict_config['provider']):
-            self.instance_params = dict_config['provider']['instance']
-        else:
-            rval = False
-        return rval
-
-    def init(self):
-        rval = True
-        _cwd = os.getcwd()
-
-        _sdict = ({"provider": {
-                       "name": "aws",
-                       "auth": {
-                           "aws_access_key_id": "",
-                           "aws_secret_access_key": ""
-                       },
-                       "instance": {
-                           "image_id": "ami-c30360aa",
-                           "key_name": "",
-                           "key_path": ""
-                       }
-                     }
-                  })
-
-        _aws_access_key_id = raw_input('enter/copy your amazon access key id: ')
-        _aws_secret_access_key = raw_input('enter/copy your amazon secret access id: ')
-        _aws_sshkey_name = raw_input('enter the name of your amazon ssh key: ')
-        _aws_sshkey_path = raw_input('enter the path to the corresponding private key (.pem file): ')
-
-        if '' not in [ _aws_access_key_id, _aws_secret_access_key, _aws_sshkey_name, _aws_sshkey_path ]:
-            _sdict['provider']['auth']['aws_access_key_id'] = _aws_access_key_id
-            _sdict['provider']['auth']['aws_secret_access_key'] = _aws_secret_access_key
-            _sdict['provider']['instance']['key_name'] = _aws_sshkey_name
-            _sdict['provider']['instance']['key_path'] = _aws_sshkey_path
-
-            try:
-                with open("{0}/{1}".format(cwd), "default.json") as jfd:
-                    json.dump(_sdict, jfd, indent=4)
-            except IOError as e:
-                sys.stderr.write("File creation failed: {0}".format(e.message))
-                rval = False
-        else:
-            sys.stderr.write("Not all required parameters have been provided.\n")
-            rval = False
-        return rval
-
-    def up(self):
-        _r = self.start()
-        if _r is not None:
-            print self.gate_hostname()
-            self.provision()
-
-        return _r
-
-    def start(self, wait=True):
-        _instance = provider.start_instance(self.conn, self.instance_params)
-       
-        if _instance is not None:
-            _istatus = 'pending'
-            while(_istatus != 'running'):
-                _istatus = self.gate_status()
-                sys.stdout.write("Wait until instance is running. Status is: {0}\n".format(_istatus))
-                time.sleep(5)
-            self.instance = _instance
-        return _instance
+    if conn is not None:
+        _instance = provider.start_instance(conn, _instance_params)
+   
+    if _instance is not None:
+        _istatus = 'pending'
+        while(_istatus != 'running' and wait):
+            _istatus = gate_status()
+            sys.stdout.write("Wait until instance is running. Status is: {0}\n".format(_istatus))
+            time.sleep(5)
+        instance = _instance
+    return _instance
 
 
-    def provision(self):
-        fabric_run(fabricant.provision(self.gate_hostname(), self.instance_params['key_path']))
+def provision():
+    pass
+ #       fabric_run(fabricant.provision(gate_hostname(), self.instance_params['key_path']))
 
-    def stop(self):
-        provider.terminate_instance(self.conn, self.instance.id)
+def stop():
+    _instance = None
 
-    def gate_status(self):
-        rval = 'halted'
+    _auth_params = LOLOLOLOL
+
+    conn = provider.cloud_connect(**_auth_params)
+    if conn is None:
+        conn = provider.cloud_connect(**_auth_params)
+    if conn and _instance is not None:
+        provider.terminate_instance(conn, _instance.id)
+
+def gate_status(instance):
+    rval = 'halted'
+    if instance:
+        instance.update()
+        rval = instance.status
+    return rval
+
+def gate_hostname(instance):
+    instance.update()
+    return instance.public_dns_name
+
+def save(instance):
+    _config_file = "{0}/{1}".format(_cwd), "vpinup.json"
+    if os.path.exists(_config_file):
+        with open(_config_file) as jfd:
+            jdict = json.load(jfd)
+
         if self.instance:
-            self.instance.update()
-            rval = self.instance.status
-        return rval
+            jdict['provider']['instance']['instance_id'] = _instance.id
+            jdict['provider']['instance']['instance_status'] = _instance.status
 
-    def gate_hostname(self):
-        self.instance.update()
-        return self.instance.public_dns_name
+        with open(_config_file, "w") as jfd:
+            json.dump(jdict, jfd)
+    else:
+        sys.stderr.write("Session could not be save")
 
-    def save(self):
-        raise NotImplementedError
